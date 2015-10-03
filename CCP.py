1785f8a543724fce83bc8221d304433b4b8475ad
biplovbhandari <bionicbiplov45@gmail.com>
2015-10-03 16:30:28 +0700

    calibration file added
    minor changes

--- CCP.py
+++ CCP.py
@@ -1,18 +1,28 @@
 """
     CCP = Camera Calibration Parameters
-    
+        - global object
+
     This file stores the CCP of different digital camera available
-    
-    Focal length is normally in mm
+
+    f is normally in mm
+    fx and fy: focal length in x and y directions
+        fx and fy exists for rectangular image (normal)
+        for square pixel size fx=fy=f
+    sx and sy: Image Size in x and y direction (pixels per millimeter)
     K1, K2 and K3: are the radial distortion parameters
     P1 and P2: are the tangential distortion parameters
+    PPx and PPy: are the Principal point coordinates
 """
 
-CCP = {"Trimble UX-5": {"Focal Length": 15.5172, # mm
+CCP = {"Trimble UX-5": {"f": 15.5172, # mm
+                        "sx": 4912,
+                        "sy": 3264,
                         "K1": -0.000202911,
                         "K2": 0.000000681845,
-                        "K3": -0.000000000864898,\
+                        "K3": -0.000000000864898,
                         "P1": -0.0000296516,
                         "P2": -0.000050424,
+                        "PPx": 0,
+                        "PPy": 0,
                         },
        }
---
+++ calibration.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+import glob
+from CCP import CCP as CCP
+
+images = glob.glob('*.JPG')
+
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # Prepare the Distortion Coefficient Matrix
+    parameters = CCP["Trimble UX-5"]
+    fx = parameters["f"] * parameters["sx"]
+    fy = parameters["f"] * parameters["sy"]
+    
+    row_1 = [fx, 0, parameters["PPx"]]
+    row_2 = [0, fy, parameters["PPy"]]
+    row_3 = [0, 0, 1]
+    cam_mtx = np.array((row_1, row_2, row_3), dtype = float)
+    dist = [[parameters["K1"], parameters["K2"], parameters["P1"], parameters["P2"], parameters["K3"]]]
+    dist = np.array(dist, dtype = float)
+    # Compensate radial and tangential lens distortion and save it
+    dst = cv2.undistort(img, cam_mtx, dist)
+    cv2.imwrite("%s_calibrated.JPG" % fname, dst)
